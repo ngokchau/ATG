bb808036b182e0af6576958d298b3c598fd5f69b
Chau Ngo <ngokchau@gmail.com>
2017-04-19 10:07:40 -0700

    Use property annotation in controller

--- public/angular/components/home/controllers/home.js
+++ public/angular/components/home/controllers/home.js
@@ -1,111 +1,113 @@
 (function() {
   'use strict';
 
-  angular.module('home').controller('homeCtrl', ['$scope', 'Vote',
-    function($scope, Vote) {
-      var totalVotes = 0;
-      var maleVotes = 0;
-      var femaleVotes = 0;
-      var malePercentage = 0;
-      var femalePercentage = 0;
+  var HomeCtrl = function($scope, Vote) {
+    var totalVotes = 0;
+    var maleVotes = 0;
+    var femaleVotes = 0;
+    var malePercentage = 0;
+    var femalePercentage = 0;
+    $scope.totalVotes = totalVotes;
+    $scope.maleVotes = maleVotes;
+    $scope.femaleVotes = femaleVotes;
+    $scope.malePercentage = malePercentage;
+    $scope.femalePercentage = femalePercentage;
+
+    /**
+     * Triggered on ng-init. Request all the necessary data to render
+     * the view. Segment out the data by gender after receiving the data
+     * from the server. This is necessary to calculate the percentage of
+     * male to female voters.
+     */
+    $scope.init = function() {
+      Vote.list()
+        .then(function(res) {
+          var votes = res.data;
+          totalVotes = votes.length;
+
+          for(var i = 0; i < totalVotes; i++) {
+            if(0 === votes[i].gender.localeCompare('m'))
+              ++maleVotes;
+            else if(0 === votes[i].gender.localeCompare('f'))
+              ++femaleVotes;
+          }
+
+          if(totalVotes > 0) {
+            malePercentage = Math.floor((maleVotes/totalVotes)*100);
+            femalePercentage = 100 - malePercentage;
+          }
+
+          updateView();
+        });
+    };
+
+    /**
+     * Triggered when user clicks the vote button to cast a vote. Checks
+     * that the input has not been modified. If so, prevent the function
+     * from executing further. Otherwise, cast the vote by triggering
+     * the manifest() function in the Vote service.
+     *
+     * If the vote has been successfully casted, when server response
+     * code is 200. Update the appropriate scope variables and
+     * recalculate percentages.
+     *
+     * @param gender
+     */
+    $scope.castVote = function(gender) {
+      if(0 !== gender.localeCompare('m') && 0 !== gender.localeCompare('f')) {
+        console.log('IllegalInputException');
+        return;
+      }
+
+      Vote.manifest(gender)
+        .then(function(res) {
+          var resIsOk = 200 === res.status;
+
+          if(!resIsOk) {
+            console.log('ServerResponseException');
+            return;
+          }
+
+          if(0 === gender.localeCompare('m'))
+            ++maleVotes;
+          else if(0 === gender.localeCompare('f'))
+            ++femaleVotes;
+
+          ++totalVotes;
+          malePercentage = Math.floor((maleVotes/totalVotes)*100);
+          femalePercentage = 100 - malePercentage;
+
+          updateView();
+        });
+    };
+
+    /**
+     * Private helper function to update the values in the $scope
+     * variable.
+     */
+    function updateView() {
       $scope.totalVotes = totalVotes;
       $scope.maleVotes = maleVotes;
       $scope.femaleVotes = femaleVotes;
-      $scope.malePercentage = malePercentage;
-      $scope.femalePercentage = femalePercentage;
+      $scope.malePercentage = malePercentage.toFixed(0) + '%';
+      $scope.femalePercentage = femalePercentage.toFixed(0) + '%';
 
-      /**
-       * Triggered on ng-init. Request all the necessary data to render
-       * the view. Segment out the data by gender after receiving the data
-       * from the server. This is necessary to calculate the percentage of
-       * male to female voters.
-       */
-      $scope.init = function() {
-        Vote.list()
-          .then(function(res) {
-            var votes = res.data;
-            totalVotes = votes.length;
-
-            for(var i = 0; i < totalVotes; i++) {
-              if(0 === votes[i].gender.localeCompare('m'))
-                ++maleVotes;
-              else if(0 === votes[i].gender.localeCompare('f'))
-                ++femaleVotes;
-            }
-
-            if(totalVotes > 0) {
-              malePercentage = Math.floor((maleVotes/totalVotes)*100);
-              femalePercentage = 100 - malePercentage;
-            }
-
-            updateView();
-          });
-      };
-
-      /**
-       * Triggered when user clicks the vote button to cast a vote. Checks
-       * that the input has not been modified. If so, prevent the function
-       * from executing further. Otherwise, cast the vote by triggering
-       * the manifest() function in the Vote service.
-       *
-       * If the vote has been successfully casted, when server response
-       * code is 200. Update the appropriate scope variables and
-       * recalculate percentages.
-       *
-       * @param gender
-       */
-      $scope.castVote = function(gender) {
-        if(0 !== gender.localeCompare('m') && 0 !== gender.localeCompare('f')) {
-          console.log('IllegalInputException');
-          return;
-        }
-
-        Vote.manifest(gender)
-          .then(function(res) {
-            var resIsOk = 200 === res.status;
-
-            if(!resIsOk) {
-              console.log('ServerResponseException');
-              return;
-            }
-
-            if(0 === gender.localeCompare('m'))
-              ++maleVotes;
-            else if(0 === gender.localeCompare('f'))
-              ++femaleVotes;
-
-            ++totalVotes;
-            malePercentage = Math.floor((maleVotes/totalVotes)*100);
-            femalePercentage = 100 - malePercentage;
-
-            updateView();
-          });
-      };
-
-      /**
-       * Private helper function to update the values in the $scope
-       * variable.
-       */
-      function updateView() {
-        $scope.totalVotes = totalVotes;
-        $scope.maleVotes = maleVotes;
-        $scope.femaleVotes = femaleVotes;
-        $scope.malePercentage = malePercentage.toFixed(0) + '%';
-        $scope.femalePercentage = femalePercentage.toFixed(0) + '%';
-
-        if(femalePercentage > malePercentage) {
-          $scope.femaleColorClass = 'text-success';
-          $scope.maleColorClass = 'text-danger';
-        }
-        else if(malePercentage > femalePercentage) {
-          $scope.femaleColorClass = 'text-danger';
-          $scope.maleColorClass = 'text-success';
-        }
-        else {
-          $scope.femaleColorClass = '';
-          $scope.maleColorClass = '';
-        }
+      if(femalePercentage > malePercentage) {
+        $scope.femaleColorClass = 'text-success';
+        $scope.maleColorClass = 'text-danger';
+      }
+      else if(malePercentage > femalePercentage) {
+        $scope.femaleColorClass = 'text-danger';
+        $scope.maleColorClass = 'text-success';
+      }
+      else {
+        $scope.femaleColorClass = '';
+        $scope.maleColorClass = '';
       }
     }
-  ]);
+  };
+
+  HomeCtrl.$inject = ['$scope', 'Vote'];
+
+  angular.module('home').controller('homeCtrl', HomeCtrl);
 })();
